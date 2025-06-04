5196fbf61ce4298c8a4447854e2fd20c1f700827
NullMeDev <192708205+NullMeDev@users.noreply.github.com>
2025-06-04 02:11:14 -0400

    Update discord.go

--- internal/util/discord.go
+++ internal/util/discord.go
@@ -1,1 +1,34 @@
+package util
 
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+// DiscordPayload is the JSON body format Discord expects.
+type DiscordPayload struct {
+	Content string `json:"content"`
+}
+
+// SendWebhook posts a simple text message to the Discord webhook.
+func SendWebhook(webhookURL, message string) error {
+	payload := DiscordPayload{Content: message}
+	bs, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	req, err := http.NewRequest("POST", webhookURL, bytes.NewBuffer(bs))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	_, err = client.Do(req)
+	return err
+}
