8eb3914962815145c0ddfed5f7f4c68beccb6a6e
NullMeDev <192708205+NullMeDev@users.noreply.github.com>
2025-06-04 01:58:22 -0400

    Update filter.go

--- internal/search/filter.go
+++ internal/search/filter.go
@@ -1,1 +1,16 @@
+package search
 
+import (
+	"time"
+
+	"github.com/NullMeDev/github-mirror-bot/internal/config"
+)
+
+func ShouldKeep(cfg *config.Config, pushedAt time.Time, stars int) bool {
+	age := time.Since(pushedAt)
+	maxAge := cfg.MaxInactive()
+	if age <= maxAge {
+		return true
+	}
+	return stars >= cfg.Filter.MinStarsForStale
+}
