9963cc76267d170f53093f083367dc8eb517aca9
NullMeDev <192708205+NullMeDev@users.noreply.github.com>
2025-06-04 01:59:42 -0400

    Update main.go

--- cmd/github-search/main.go
+++ cmd/github-search/main.go
@@ -1,1 +1,29 @@
+package main
 
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/NullMeDev/github-mirror-bot/internal/config"
+	"github.com/NullMeDev/github-mirror-bot/internal/search"
+)
+
+func main() {
+	cfg, err := config.Load("/home/gitbackup/github-mirror-bot/config.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	token := os.Getenv(cfg.GitHub.TokenEnv)
+	if token == "" {
+		log.Fatal("GITHUB_TOKEN env variable not set")
+	}
+
+	q := search.NewQueue("127.0.0.1:6379")
+	s := search.NewSearcher(cfg, token, q)
+	if err := s.Run(context.Background()); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Search cycle complete")
+}
